59f5ee9b2b733b64fe789a2e96c0b2da900b5d6e
Reshaun-Dawson <reshaun.dawson@gmail.com>
2026-02-16 21:37:16 -0500

    chore(deploy): publish to gh-pages

--- components/supabase-client.ts
+++
@@ -1,5 +0,0 @@
-import {createClient} from '@supabase/supabase-js';
-export const supabase = createClient(
-  "https://bstqpalyjfmzsnpdoqpw.supabase.co",
-  "sb_publishable_RVRSQSENiwNK92Mt-da1ww__khxDukv"
-);
